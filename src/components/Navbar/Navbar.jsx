8027cf95f399977332a497442780038067d94a7d
bhawana <bhawanayadav0907@gmail.com>
2025-07-28 21:44:32 +0530

    fixed the import of supabase in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { Menu, X } from 'lucide-react';
-import { supabase } from '../../supabaseClient'; // adjust path as needed
+import { supabase } from './../../../supabaseClient';
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
