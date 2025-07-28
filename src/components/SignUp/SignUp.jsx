eb7bd2304eaff8d34f7f65e4c0af74286808dd0a
bhawana <bhawanayadav0907@gmail.com>
2025-07-28 21:42:12 +0530

    fixed import of supabase

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from './../../../supabaseClient';
 
 const Signup = () => {
   const [email, setEmail] = useState('');
